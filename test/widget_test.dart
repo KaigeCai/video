f84e9b01e8a3fe71d202b6b8fe0696a728c1f2a4
Kaige Cai <caikaige.g@gmail.com>
2025-01-18 21:20:11 +0800

    .

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:video/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const VideoPlayerApp());
+    await tester.pumpWidget(const MaterialApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
